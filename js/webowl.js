821e8bc26146070d30d206ef4487e476667435d1
diasro <dias.robert@gmail.com>
2014-06-27 07:41:34 -0700

    Added background music and redesign game board with basic animation

--- js/webowl.js
+++ js/webowl.js
@@ -1,4 +1,6 @@
 (function(webowl, $, undefined) {
+	var RANDOM_DEGREE = 90;
+	
 	function loadMenu() {
 		$.ajax({
 			url : "./menu.html",
@@ -10,18 +12,53 @@
 		});
 	}
 	
+	function drop(degree,pin_no) {
+		var $pin = $("#pin" + pin_no);
+		$pin.css({ WebkitTransform: 'rotate(' + degree + 'deg)'});
+		$pin.css({ '-moz-transform': 'rotate(' + degree + 'deg)'});
+		if(degree < RANDOM_DEGREE){
+			setTimeout(function() { drop(++degree,pin_no); },3);
+		}
+    }
+	
+	webowl.dropPins=function(pinarry) {
+		animateBall(pinarry);
+	};
+	
+	function animateBall(pinarry) {
+		var degree = -9;
+		$("#ballpath").css("right","-50");		
+		$("#ballpath").animate({
+			opacity: 100,
+		    right: "+=900",
+		    step: function( now, fx ) {
+		    	degree = degree - 5;
+		    	$("#ballpath").css({ '-moz-transform': 'rotate(' + degree + 'deg)'});	
+		    	$("#ballpath").css({ WebkitTransform: 'rotate(' + degree + 'deg)'});
+		      }		    
+		  }, 2000, function() {			  		 
+				for (var i = 0; i <= pinarry.length; i++) {
+					drop(0,pinarry[i]);
+				}
+		  }
+		  );
+	}
+	
+	function loadView(view) {		
+		$.ajax({
+			url : "./" + view + ".html",
+			type : "GET",
+			dataType : "html",
+			success : function(data) {
+				$("#bowlcontainer").html(data);
+			}
+		});		
+	}
+	
 	function registerMenuEvents() {
 		$(document).off('click', '.mnu').on( 'click', '.mnu', function (event) {
 			event.preventDefault();
-			var pagesrc=$(this).attr('id');
-			$.ajax({
-				url : "./" + pagesrc + ".html",
-				type : "GET",
-				dataType : "html",
-				success : function(data) {
-					$("#bowlcontainer").html(data);
-				}
-			});
+			loadView($(this).attr('id'));
 		});
 		
 	}
@@ -29,6 +66,11 @@
 	webowl.init=function() {
 		registerMenuEvents();
 		loadMenu();
+		$(document).off('click', '#nxtball').on( 'click', '#nxtball', function (event) {
+			event.preventDefault();
+			loadView('playfield');
+		});		
+
 	};
 
 }(window.webowl = window.webowl || {}, jQuery));
